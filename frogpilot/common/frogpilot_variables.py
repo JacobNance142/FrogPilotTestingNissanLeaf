369f027447609e1fdcb4bddb1f6d88093a9d562b
JacobNance142 <majora.142@outlook.com>
2026-04-08 19:49:38 -0400

    Change the threshold to 1.0 from 0.63 for stop light/sign detection

--- frogpilot/common/frogpilot_variables.py
+++ frogpilot/common/frogpilot_variables.py
@@ -42,7 +42,7 @@ EARTH_RADIUS = 6378137                    # Radius of the Earth in meters
 MAX_T_FOLLOW = 3.0                        # Maximum allowed following duration. Larger values risk losing track of the lead but may be increased as models improve
 MINIMUM_LATERAL_ACCELERATION = 1.3        # m/s^2, typical minimum lateral acceleration when taking curves
 PLANNER_TIME = ModelConstants.T_IDXS[-1]  # Length of time the model projects out for
-THRESHOLD = 0.63                          # Requires the condition to be true for ~1 second
+THRESHOLD = 1.0                          # Requires the condition to be true for ~1 second
 
 NON_DRIVING_GEARS = [GearShifter.neutral, GearShifter.park, GearShifter.reverse, GearShifter.unknown]
 
